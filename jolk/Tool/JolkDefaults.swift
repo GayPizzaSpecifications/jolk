a03cf88480ad7c0f7b146eb443179dcc53cd8192
Kenneth Endfinger <kaendfinger@gmail.com>
2020-12-28 21:45:23 -0500

    Reorganization and CLeanup

---
+++ jolk/Tool/JolkDefaults.swift
@@ -0,0 +1,23 @@
+//
+//  JolkDefaults.swift
+//  jolk
+//
+//  Created by Kenneth Endfinger on 12/28/20.
+//
+
+import Foundation
+
+struct JolkDefaults {
+    static let standardExecutablePaths = [
+        "/bin",
+        "/sbin",
+        "/usr/bin",
+        "/usr/sbin",
+        "/usr/libexec"
+    ]
+
+    static let standardLaunchdPaths = [
+        "/System/Library/LaunchAgents",
+        "/System/Library/LaunchDaemons"
+    ]
+}
---
+++ jolk/Tool/JolkTool.swift
@@ -0,0 +1,108 @@
+//
+//  JolkTool.swift
+//  jolk
+//
+//  Created by Kenneth Endfinger on 12/28/20.
+//
+
+import ArgumentParser
+import Foundation
+
+struct JolkTool: ParsableCommand {
+    @Flag(help: "Quiet Mode")
+    var quiet: Bool = false
+    
+    @Option(name: .shortAndLong, help: "Maximum Current Jobs")
+    var jobs: Int = 12
+    
+    @Option(name: .shortAndLong, help: "Output File")
+    var output: String = "executables.json"
+    
+    func run() throws {
+        let executableCollector = ExecutableCollector()
+        for filePath in JolkDefaults.standardExecutablePaths {
+            try executableCollector.scan(filePath)
+        }
+        executableCollector.sort()
+
+        let launchdCollector = LaunchdCollector()
+        for filePath in JolkDefaults.standardLaunchdPaths {
+            try launchdCollector.scan(filePath)
+        }
+
+        let analyzers: [ExecutableAnalyzer] = [
+            LipoAnalyzer(),
+            StringsAnalyzer(),
+            ManPageAnalyzer(),
+            DynamicLinkerAnalyzer(),
+            UsageExtractionAnalyzer(),
+            try LaunchdAnalyzer(launchdCollector)
+        ]
+
+        let outputCollection = AnalysisOutputCollection()
+
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = jobs
+        for urlForExecutable in executableCollector.executables {
+            let url = urlForExecutable
+            let outputForFile = outputCollection.get(url)
+            queue.addOperation {
+                analyze(url, analyzers, outputForFile)
+            }
+        }
+
+        queue.addBarrierBlock {
+            save(outputCollection)
+        }
+
+        dispatchMain()
+    }
+    
+    func analyze(_ url: URL, _ analyzers: [ExecutableAnalyzer], _ output: AnalysisOutput) {
+        let start = DispatchTime.now()
+        if !quiet {
+            print("analyze \(url.path)")
+        }
+        for analyzer in analyzers {
+            do {
+                try analyzer.analyze(url, output)
+                if !output.isInCollection() {
+                    if !quiet {
+                        print("kick \(url.path)")
+                    }
+                    break
+                }
+            } catch {
+                print("error in analyzer \(analyzer.name()) for path \(url.path): \(error)")
+            }
+        }
+        let end = DispatchTime.now()
+        let timeInNanoseconds = end.uptimeNanoseconds - start.uptimeNanoseconds
+        let ms = Double(timeInNanoseconds) / 1_000_000.0
+        if !quiet {
+            print("complete \(url.path) \(String(format: "%.2f", ms))ms")
+        }
+    }
+    
+    func save(_ outputCollection: AnalysisOutputCollection) {
+        do {
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.dataEncodingStrategy = .base64
+            jsonEncoder.outputFormatting = [
+                .prettyPrinted,
+                .withoutEscapingSlashes,
+                .sortedKeys
+            ]
+            let result = try jsonEncoder.encode(outputCollection.encode())
+            let resultToString = String(data: result, encoding: .utf8)!
+            try resultToString.write(
+                to: URL(fileURLWithPath: self.output),
+                atomically: true,
+                encoding: .utf8
+            )
+        } catch {
+            JolkTool.exit(withError: error)
+        }
+        JolkTool.exit()
+    }
+}
